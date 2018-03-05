724447e232824cd8fa818905b06133899121e5c1
Tup Luisking <ramon.luisking14@gmail.com>
2018-03-05 22:03:33 +0800

    REMAKE

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Script imported");
+
+function changeImage(element, newSrc) {
+    
+    element.setAttribute('src', "images/art/" + newSrc);
+}
+
+function openGame(url) {
+    
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+
+function changePortrait(element, newSrc) {
+    
+    element.setAttribute('src', "images/" + newSrc);
+}
