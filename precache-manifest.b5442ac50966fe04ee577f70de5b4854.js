8628f78212256809af3d4f768dd01b5f19555eb3
TupLuisking <ramon.luisking14@gmail.com>
2020-10-29 20:32:58 +0800

    Updates

---
+++ precache-manifest.b5442ac50966fe04ee577f70de5b4854.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c2ebe5713f13f8ab12b8567771edac",
+    "url": "/game-folio/index.html"
+  },
+  {
+    "revision": "e585a23699822b503c8e",
+    "url": "/game-folio/static/css/main.7a034755.chunk.css"
+  },
+  {
+    "revision": "b76557fee87b6fd07c4d",
+    "url": "/game-folio/static/js/2.0b39050f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-folio/static/js/2.0b39050f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e585a23699822b503c8e",
+    "url": "/game-folio/static/js/main.84ee9b8d.chunk.js"
+  },
+  {
+    "revision": "b926692784283d5e7e34",
+    "url": "/game-folio/static/js/runtime-main.1661efab.js"
+  },
+  {
+    "revision": "51d9255fff4aaa8a3a40570bcdd44f05",
+    "url": "/game-folio/static/media/SS1.51d9255f.png"
+  },
+  {
+    "revision": "5dbd98ac7192b6a0fadcb6fcee36c11d",
+    "url": "/game-folio/static/media/SS1.5dbd98ac.png"
+  },
+  {
+    "revision": "8108330a5f411a1f657499878fa02a0b",
+    "url": "/game-folio/static/media/SS1.8108330a.png"
+  },
+  {
+    "revision": "96d66009c7e9de46aab2b095b3dfb907",
+    "url": "/game-folio/static/media/SS1.96d66009.png"
+  },
+  {
+    "revision": "e4a742d4ab6e2bb6a765d104983d24a8",
+    "url": "/game-folio/static/media/SS1.e4a742d4.png"
+  },
+  {
+    "revision": "e547c92a25b53af0f2dd1d20b228f1cb",
+    "url": "/game-folio/static/media/SS1.e547c92a.png"
+  },
+  {
+    "revision": "f61704eb21a98cf07f4f0bd2d945c595",
+    "url": "/game-folio/static/media/SS1.f61704eb.png"
+  },
+  {
+    "revision": "631ea3e303608006039aaad2cfd24ff1",
+    "url": "/game-folio/static/media/SS2.631ea3e3.png"
+  },
+  {
+    "revision": "96bd13dd13d87b21e73d6f6cf343fa4e",
+    "url": "/game-folio/static/media/SS2.96bd13dd.png"
+  },
+  {
+    "revision": "cea92a481268c7def88f8ce4a4d6afc5",
+    "url": "/game-folio/static/media/SS2.cea92a48.png"
+  },
+  {
+    "revision": "dbb18a689a7fca943bbcd220c108725b",
+    "url": "/game-folio/static/media/SS2.dbb18a68.png"
+  },
+  {
+    "revision": "dbe95de4310184a306381cc574ec371d",
+    "url": "/game-folio/static/media/SS2.dbe95de4.png"
+  },
+  {
+    "revision": "f2b0be13295c58aab5c683c0b32d334b",
+    "url": "/game-folio/static/media/SS2.f2b0be13.png"
+  },
+  {
+    "revision": "fd75f60388f346f163df6b9d389389e4",
+    "url": "/game-folio/static/media/SS2.fd75f603.png"
+  },
+  {
+    "revision": "30628fccae5009814d271d65a10ce790",
+    "url": "/game-folio/static/media/SS3.30628fcc.png"
+  },
+  {
+    "revision": "8c4359dd18ced566e3dcf983f216428f",
+    "url": "/game-folio/static/media/SS3.8c4359dd.png"
+  },
+  {
+    "revision": "af13ce4aef73b31c8b7d08376add27f5",
+    "url": "/game-folio/static/media/SS3.af13ce4a.png"
+  },
+  {
+    "revision": "b282113d1e27b68b344414aea5322192",
+    "url": "/game-folio/static/media/SS3.b282113d.png"
+  },
+  {
+    "revision": "c027061e57123605281072c0860d6498",
+    "url": "/game-folio/static/media/SS3.c027061e.png"
+  },
+  {
+    "revision": "c0f9e7b1173df0119105fad175c9d08b",
+    "url": "/game-folio/static/media/SS3.c0f9e7b1.png"
+  },
+  {
+    "revision": "ec5f701d83b2ffde5c80a4a7cff4fb7e",
+    "url": "/game-folio/static/media/SS3.ec5f701d.png"
+  },
+  {
+    "revision": "5f3c2beae81d7b31e01976e14bb0a7db",
+    "url": "/game-folio/static/media/SS4.5f3c2bea.png"
+  },
+  {
+    "revision": "86f99f56327e4a22e4597c6f494b18f0",
+    "url": "/game-folio/static/media/SS4.86f99f56.png"
+  },
+  {
+    "revision": "aa0900361977a75702edbb3ee3b1970f",
+    "url": "/game-folio/static/media/SS4.aa090036.png"
+  },
+  {
+    "revision": "beb66535fde12c52c695882b47e7fb13",
+    "url": "/game-folio/static/media/SS4.beb66535.png"
+  },
+  {
+    "revision": "ef66e29f84373e9ed0e7cc336014e170",
+    "url": "/game-folio/static/media/SS4.ef66e29f.png"
+  },
+  {
+    "revision": "a546697c78b9924fc40d2a7f68994545",
+    "url": "/game-folio/static/media/SS5.a546697c.png"
+  },
+  {
+    "revision": "de61592b0af34903c4b7ad9877883760",
+    "url": "/game-folio/static/media/SS5.de61592b.png"
+  },
+  {
+    "revision": "f1bd71b26b405b39a32667e7a5ebb8b7",
+    "url": "/game-folio/static/media/SS5.f1bd71b2.png"
+  },
+  {
+    "revision": "c0b79496551cf2b7377738679fae3fc9",
+    "url": "/game-folio/static/media/SS6.c0b79496.png"
+  },
+  {
+    "revision": "d79ebdfbbf2fde306726267eed43cdeb",
+    "url": "/game-folio/static/media/admu.d79ebdfb.jpg"
+  },
+  {
+    "revision": "d467401b5a97c814e852bf46f0ca6b4d",
+    "url": "/game-folio/static/media/anino.d467401b.jpg"
+  },
+  {
+    "revision": "62e84f8271d32eb452129a0b7d59540d",
+    "url": "/game-folio/static/media/antipasto-regular.62e84f82.otf"
+  },
+  {
+    "revision": "4da07f1dc8a1462bc9e697c2a2ce690a",
+    "url": "/game-folio/static/media/ballgame.4da07f1d.png"
+  },
+  {
+    "revision": "21bb70b62317f276f2e97a919ff5bd8c",
+    "url": "/game-folio/static/media/bebas-neue.21bb70b6.ttf"
+  },
+  {
+    "revision": "a043b615199382b0e049242e84ad70e8",
+    "url": "/game-folio/static/media/bh2016.a043b615.jpg"
+  },
+  {
+    "revision": "ec5f701d83b2ffde5c80a4a7cff4fb7e",
+    "url": "/game-folio/static/media/captain.ec5f701d.png"
+  },
+  {
+    "revision": "a146d899b89aa3cee95d3449ed022fee",
+    "url": "/game-folio/static/media/compsat.a146d899.jpg"
+  },
+  {
+    "revision": "1dd523688d9dab3d22ed4d2f14842349",
+    "url": "/game-folio/static/media/compsat2.1dd52368.jpg"
+  },
+  {
+    "revision": "394b853d38c0fcbcb82cbc44a9d28d4f",
+    "url": "/game-folio/static/media/disclaimer.394b853d.png"
+  },
+  {
+    "revision": "58212e86e087b7fb845218163753dad3",
+    "url": "/game-folio/static/media/disclaimer.58212e86.png"
+  },
+  {
+    "revision": "3a52f9f36364c02d5372b86f87047eca",
+    "url": "/game-folio/static/media/ggj.3a52f9f3.jpg"
+  },
+  {
+    "revision": "8e79476250b10f51162de0d2b98d76c4",
+    "url": "/game-folio/static/media/ggj2-2018.8e794762.jpg"
+  },
+  {
+    "revision": "216ba18ed9ba35930c56a288828e22cc",
+    "url": "/game-folio/static/media/ggj2-2019.216ba18e.jpg"
+  },
+  {
+    "revision": "328812d16cf68f7690239039056f4ee5",
+    "url": "/game-folio/static/media/ggj2018.328812d1.png"
+  },
+  {
+    "revision": "83328afcb0e1e45030b57c4ab87f007e",
+    "url": "/game-folio/static/media/ggj2018.83328afc.png"
+  },
+  {
+    "revision": "bd4c5f2d3a23c2a526ad8380a974f67a",
+    "url": "/game-folio/static/media/ggj2019.bd4c5f2d.png"
+  },
+  {
+    "revision": "dbe95de4310184a306381cc574ec371d",
+    "url": "/game-folio/static/media/ggj2019.dbe95de4.png"
+  },
+  {
+    "revision": "69b65929f249827f612a1be959faba31",
+    "url": "/game-folio/static/media/hbg.69b65929.png"
+  },
+  {
+    "revision": "934813d4a204cba8b0e0e00dca5329d4",
+    "url": "/game-folio/static/media/laptop.934813d4.jpg"
+  },
+  {
+    "revision": "10fee3db59c12688833f0561e533ca8d",
+    "url": "/game-folio/static/media/literallyball.10fee3db.png"
+  },
+  {
+    "revision": "433cbeeeb883f15401493925cad8c9a5",
+    "url": "/game-folio/static/media/monstronauts2.433cbeee.jpg"
+  },
+  {
+    "revision": "5d5ea206efcec2ee8f64840703e9754d",
+    "url": "/game-folio/static/media/peteredout.5d5ea206.png"
+  },
+  {
+    "revision": "09662cc0163f637c4cb1861de8aa2b87",
+    "url": "/game-folio/static/media/portrait.09662cc0.png"
+  },
+  {
+    "revision": "ec6b0b31cb6f4f72b3c75f1a4fdbc315",
+    "url": "/game-folio/static/media/random.ec6b0b31.jpg"
+  },
+  {
+    "revision": "c481b62f8899c7a19e91c14ef429257d",
+    "url": "/game-folio/static/media/salinlahi.c481b62f.jpg"
+  },
+  {
+    "revision": "3335aaa817b7e0ee146c154126793c18",
+    "url": "/game-folio/static/media/sushi.3335aaa8.png"
+  },
+  {
+    "revision": "11c17bd18f8477ea61887540c41139e8",
+    "url": "/game-folio/static/media/sushigame.11c17bd1.png"
+  },
+  {
+    "revision": "b74e59e372a6e9c2c118817f550b5d9c",
+    "url": "/game-folio/static/media/thesis.b74e59e3.jpg"
+  }
+]);
---
+++ script.js
@@ -0,0 +1,157 @@
+console.log("Script Imported");
+
+var isHomePage;
+
+// NAVBAR FIELDS
+
+var navbar;
+var navbarLogoContainer;
+var navbarHeight;
+var navbarLinks;
+var navbarBurgerSticks;
+var navbarBurgerLinksContainer;
+var navbarTransitionDuration;
+
+// GAMES PAGE FIELDS
+
+var gameGalleryItems;
+var gameGalleryItemGap;
+var gameGalleryItemWidth;
+var gameDetailsSection;
+var currentGameIndex = 0;
+var gameDetailsAnimationDuration = 0.125 * 1000;
+
+window.onload = function ()
+{
+	navbar = $(".navbar");
+	navbarLogoContainer = $(".navbar-logo-container");
+	navbarHeight = navbar.height();
+	navbarLinks = document.getElementsByClassName("navbar-link");
+	navbarBurgerSticks = document.getElementsByClassName("navbar-burger-stick");
+	navbarBurgerLinksContainer = $(".navbar-burger-links-container");
+	navbarTransitionDuration = "0.69s";
+
+	isHomePage = (window.location.pathname === "/");
+
+	var wScroll = $(this).scrollTop();
+	updateNavbar(wScroll);
+
+	gameDetailsSection = $('.game-details-section');
+	gameGalleryItems = document.getElementsByClassName("games-gallery-item");
+	if (gameGalleryItems !== null && gameGalleryItems.length !== 0)
+	{
+		gameGalleryItemWidth = gameGalleryItems[0].offsetWidth;
+		gameGalleryItemGap = gameGalleryItemWidth * 0.1;
+	}
+
+	AdjustGameGallery(0);
+}
+
+$(window).scroll(function ()
+{
+	var wScroll = $(this).scrollTop();
+	updateNavbar(wScroll);
+});
+
+function test()
+{
+	console.log("TEST");
+}
+
+function updateNavbar(wScroll)
+{
+	if (navbar === null)
+		return;
+
+	var simple = isHomePage && wScroll < navbarHeight * 3;
+
+	if (isHomePage)
+	{
+		navbar.css("transition-duration", navbarTransitionDuration);
+		navbarLogoContainer.css("transition-duration", navbarTransitionDuration);
+	}
+
+	//navbar.css("border-bottom", simple ? "none" : "1px solid black");
+
+	navbar.css("background", simple ? "rgba(0,0,0,0)" : "#f0f8ff");
+	navbar.css("box-shadow", simple ? "none" : "0px -10px 15px 10px rgba(0,0,0,0.5)");
+
+	navbarBurgerLinksContainer.css("background", simple ? "rgba(0,0,0,0)" : "#d5dde3");
+	
+	navbarLogoContainer.css("opacity", simple ? "0" : "1");
+	navbarLogoContainer.css("flex-grow", simple ? "0" : "1");
+	navbarLogoContainer.css("transition-timing-function", simple ? "ease-in" : "ease-out");
+
+	if (navbarLinks != null)
+	{
+		for (var i = 0; i < navbarLinks.length; i++)
+		{
+			navbarLinks[i].style.color = simple ? "white" : "black";
+		}
+	}
+	
+	if (navbarBurgerSticks != null)
+	{
+		for (var i = 0; i < navbarBurgerSticks.length; i++)
+		{
+			navbarBurgerSticks[i].style.borderBottomColor = simple ? "white" : "black";
+		}
+	}
+}
+
+function AdjustGameGallery(index)
+{
+	if (gameGalleryItems === null)
+		return;
+	
+	if (currentGameIndex !== index)
+	{
+		// Animate
+		gameDetailsSection.css("transition", "0s all");
+		gameDetailsSection.css("opacity", "0");
+		setTimeout(() =>
+		{
+			gameDetailsSection.css("transition", "1s all");
+			gameDetailsSection.css("opacity", "1");
+		}, gameDetailsAnimationDuration);
+	}
+
+	currentGameIndex = index;
+
+	for (var i = 0; i < gameGalleryItems.length; i++)
+	{
+		var item = gameGalleryItems[i];
+		var offsetIndex = i - currentGameIndex;
+
+		var xPos = (offsetIndex * (gameGalleryItemWidth + gameGalleryItemGap));
+		var scale = i !== index ? 1 : 1.125;
+
+		var offsetPos = 0;
+
+		if (i < currentGameIndex)
+			offsetPos = -gameGalleryItemGap;
+		else if (i > currentGameIndex)
+			offsetPos = gameGalleryItemGap;
+
+		xPos += offsetPos;
+
+		var xPosString = xPos + "px";
+
+		item.style.transform =
+			"translateX(" + xPosString + ")" +
+			"scale(" + scale + ")";
+	}
+}
+
+function CloseBurgerMenu()
+{
+	console.log("close burger menu");
+	$(".navbar-burger-toggle").prop("checked", false);
+}
+
+function RevalidateBurgerMenu()
+{
+	var checked = $(".navbar-burger-toggle").prop("checked");
+	
+	navbarBurgerLinksContainer.css("max-height", checked ? "400px" : "0px");
+}
